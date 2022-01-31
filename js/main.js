cbe3fbb9ce5c063fb1c54a87d0e3b2ba57adf3fb
Usmonkul <56948891+Usmonkul@users.noreply.github.com>
2022-01-31 11:50:43 +0900

    fix the javascript code

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 "use strict";
 
-const elhidden = document.querySelector(".directors-info-hidden");
+const elhidden = document.querySelectorAll(".directors-info-hidden");
 const eltogger = document.querySelectorAll(".plus-toggler");
 const elplusImg = document.querySelectorAll(".plus-img");
-for (let i = 0; i < eltogger.length; i++)
-  eltogger[i].addEventListener("click", function () {
-    elhidden.classList.toggle("hidden");
-    eltogger[i].classList.toggle("toggled-state");
-    elplusImg[5].classList.toggle("plus-toggled");
+
+eltogger.forEach((Element, index) => {
+  Element.addEventListener("click", function () {
+    Element.classList.toggle("toggled-state");
+    elhidden[index].classList.toggle("hidden");
+    elplusImg[index].classList.toggle("plus-toggled");
   });
+});
