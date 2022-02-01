efd86b012a164bc716e8e36c9ae47f1c21acff34
Usmonkul <56948891+Usmonkul@users.noreply.github.com>
2022-02-01 16:27:07 +0900

    fix scroll issues on mobile navbar

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
 const elhidden = document.querySelectorAll(".directors-info-hidden");
 const eltogger = document.querySelectorAll(".plus-toggler");
 const elplusImg = document.querySelectorAll(".plus-img");
+const elbodyScroll = document.querySelector("body");
 
 eltogger.forEach((Element, index) => {
   Element.addEventListener("click", function () {
@@ -18,6 +19,7 @@ const elhamBtn = document.querySelector(".hamburger-menu");
 const elmobileNav = document.querySelector(".mobile-nav");
 
 elhamBtn.addEventListener("click", function () {
+  elbodyScroll.classList.toggle("nav-scroll-hidden");
   elhamBtn.classList.toggle("humburger-closed");
   elmobileNav.classList.toggle("mobile-nav-active");
 });
